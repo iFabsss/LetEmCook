29897553c3334ba100c758c21e8cb905d8790192
iFabsss <guarinfabian7@gmail.com>
2026-04-18 02:35:33 +0800

    Create Superuser on build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py createmigrations
+python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsu
+python manage.py createsuperuser --no-input || true
